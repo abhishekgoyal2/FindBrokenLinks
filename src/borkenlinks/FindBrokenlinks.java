2dc9e2d8615ce703c8dc9b62ed295b724135de18
abhishek.goyal2 <abhi25goel@gmail.com>
2018-05-22 17:25:26 +0530

    Find Broken links

---
+++ src/borkenlinks/FindBrokenlinks.java
@@ -0,0 +1,15 @@
+package borkenlinks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class FindBrokenlinks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new FirefoxDriver();
+
+driver.get("http://www.learn-ausdas.com/");
+	}
+
+}
